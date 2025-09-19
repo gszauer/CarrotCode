340ba31e61a38f1ef29682d220d938343af3599b
Gabor <git@gabormakesgames.com>
2025-09-19 15:05:26 -0700

    Refactored linux api

--- code/linux.cpp
+++ code/linux.cpp
@@ -1,4 +1,3 @@
-#include "linux.h"
 #include "application.h"
 #include "platform.h"
 #include "strings.h"
@@ -6,6 +5,38 @@
 #include <map>
 #include <string>
 #include <cstring>
+
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xos.h>
+#include <X11/Xatom.h>
+#include <X11/keysym.h>
+#include <sys/time.h>
+
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <fstream>
+#include <vector>
+#include <cctype>
+
+#include "renderer.h"
+#include "strings.h"
+#include "document.h"
+#include "imgui.h"
+
+struct WindowData {
+    Display* display;
+    Window window;
+    GC gc;
+    XImage* backBuffer;
+    unsigned int* pixels;
+    int screen;
+    int width;
+    int height;
+    bool closeWindow;
+};
 
 #define WINDOW_WIDTH 1600
 #define WINDOW_HEIGHT 1200
